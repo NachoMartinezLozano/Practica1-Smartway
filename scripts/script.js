44357c0176195128a4aa1ce0724a2408fd023e98
NachoMartinezLozano <joseignacio.martinezl@um.es>
2025-06-11 12:54:45 +0200

    Añadido carrusel, fallo modo oscuro

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 
 document.getElementById('modo-oscuro-toggle').addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+    document.body.classList.toggle('modo-oscuro');
+});
+
+
+// Fijar el scroll de la pantalla cuando nos vemos en el carousel de imágenes
+document.querySelectorAll('.carousel .btn-circle').forEach(button => {
+button.addEventListener('click', function (event) {
+    event.preventDefault(); // Evita el scroll automático
+    const targetId = this.getAttribute('href').substring(1); // obtiene el ID (sin #)
+    const target = document.getElementById(targetId);
+    if (target) {
+    target.scrollIntoView({ behavior: 'instant', block: 'nearest' }); // opcional: ajusta el comportamiento si necesitas animación
+    }
+});
 });
